e8c7e58c118479272cfdb4d7a984a63934f271da
Iddo Lev <iddolev9@gmail.com>
2026-03-17 23:56:42 +0200

    .gitignore

--- apf_install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM This is the file to share with anyone who wants to install APF in their project folder
-
-curl -sL -o apf_install.py https://raw.githubusercontent.com/iddolev/apf/main/installation/apf_install.py
-python apf_install.py
